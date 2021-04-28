42f90320da6e970fe6593a6d0baee5587dbe3f0e
Sebastian Rodriguez <jsrodriguez6536@misena.edu.co>
2021-04-28 15:42:50 +0200

    Proyecto creado

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+//funcion anonima
+//callback
+Route::get('variables', function(){
+    //definir una variable
+    //en php
+    $mensaje = 20;
+    //funcion var_dump:analizar una variable
+    //muestra tipo de dato y valor
+    print_r($mensaje);
+    echo"<hr />";
+    $mensaje=true;
+    var_dump($mensaje);
+});
+Route:: get('arreglos' , function(){
+    //Arreglo: estructura de datos
+    $estudiantes = ["AN"=>"Ana" ,
+                    "MA"=>"Maria" ,
+                    "SE"=>"Sebas"];
+        echo"<pre>";
+    var_dump($estudiantes);
+        echo"</pre>";
+});
